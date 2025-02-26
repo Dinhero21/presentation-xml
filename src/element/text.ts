de78369fa8b93bb866aa8877584cf0bc2562f08d
Dinhero21 <dinhero21@dinhero21.dev>
2025-02-26 02:18:16 -0300

    better text

--- src/element/text.ts
+++ src/element/text.ts
@@ -1,6 +1,25 @@
 import { PresentationElement, registerElement } from './index.js';
 
+export const TEXTUAL_SYMBOL = Symbol('element.type=text');
+
+declare global {
+  interface Node {
+    [TEXTUAL_SYMBOL]?: boolean;
+  }
+}
+
+export function isText(element: Element | null): boolean {
+  while (element !== null) {
+    if (element[TEXTUAL_SYMBOL] === true) return true;
+    element = element.parentElement;
+  }
+
+  return false;
+}
+
 export function renderText(element: PresentationElement) {
+  element.original[TEXTUAL_SYMBOL] = true;
+
   element.custom.set('bold', (element) => {
     const b = document.createElement('b');
     element.renderInto(b);
@@ -22,20 +41,22 @@ export function renderText(element: PresentationElement) {
     }[element.getTheme()] ?? '';
   element.default.size ??= '3rem';
 
-  const p = document.createElement('p');
-  element.renderInto(p);
+  const inText = isText(element.original.parentElement);
+
+  const text = document.createElement(inText ? 'span' : 'p');
+  element.renderInto(text);
 
   const color = element.getAttribute('color');
   const size = element.getAttribute('size');
   const weight = element.getAttribute('weight');
   const family = element.getAttribute('family');
 
-  if (color !== null) p.style.color = color;
-  if (size !== null) p.style.fontSize = size;
-  if (weight !== null) p.style.fontWeight = weight;
-  if (family !== null) p.style.fontFamily = family;
+  if (color !== null) text.style.color = color;
+  if (size !== null) text.style.fontSize = size;
+  if (weight !== null) text.style.fontWeight = weight;
+  if (family !== null) text.style.fontFamily = family;
 
-  return p;
+  return text;
 }
 
 registerElement('text', renderText);
